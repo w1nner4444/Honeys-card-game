fc7239bce254df62fd346319fdc5d1b693e81548
HUNTER FORBES <u0830157@umail.utah.edu>
2019-10-05 16:21:54 -0600

    Set up some base formating for the forms with descriptor code that will update display tables

--- Get8/Get8Backbone/Form1.Designer.cs
+++ Get8/Get8Backbone/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Card_Table = new System.Windows.Forms.TableLayoutPanel();
+            this.Trade_Button = new System.Windows.Forms.Button();
+            this.Turn_Table = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // Card_Table
+            // 
+            this.Card_Table.ColumnCount = 2;
+            this.Card_Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Card_Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Card_Table.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card_Table.Location = new System.Drawing.Point(12, 97);
+            this.Card_Table.Name = "Card_Table";
+            this.Card_Table.RowCount = 17;
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Card_Table.Size = new System.Drawing.Size(188, 340);
+            this.Card_Table.TabIndex = 0;
+            // 
+            // Trade_Button
+            // 
+            this.Trade_Button.Location = new System.Drawing.Point(236, 155);
+            this.Trade_Button.Name = "Trade_Button";
+            this.Trade_Button.Size = new System.Drawing.Size(141, 78);
+            this.Trade_Button.TabIndex = 2;
+            this.Trade_Button.Text = "Trade";
+            this.Trade_Button.UseVisualStyleBackColor = true;
+            this.Trade_Button.Click += new System.EventHandler(this.Trade_Button_Click);
+            // 
+            // Turn_Table
+            // 
+            this.Turn_Table.ColumnCount = 1;
+            this.Turn_Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Turn_Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Turn_Table.Location = new System.Drawing.Point(533, 141);
+            this.Turn_Table.Name = "Turn_Table";
+            this.Turn_Table.RowCount = 1;
+            this.Turn_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Turn_Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Turn_Table.Size = new System.Drawing.Size(100, 129);
+            this.Turn_Table.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(723, 564);
+            this.Controls.Add(this.Turn_Table);
+            this.Controls.Add(this.Trade_Button);
+            this.Controls.Add(this.Card_Table);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +105,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel Card_Table;
+        private System.Windows.Forms.Button Trade_Button;
+        private System.Windows.Forms.TableLayoutPanel Turn_Table;
     }
 }
 
---
+++ Get8/Get8Backbone/Trade_Window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Get8Backbone
+{
+    public partial class Trade_Window : Form
+    {
+        public Trade_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
